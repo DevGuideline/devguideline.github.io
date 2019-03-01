e048574aefc1fd9ce5e47a55a342610ac0876f5c
zhoucs <zhoucs@zhoucsdeAir.localdomain>
2019-03-01 15:01:51 +0800

    update app frame

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,19 @@
-import React from "react"
+import React from 'react'
+import { Helmet } from 'react-helmet'
 
-export default () => <div>Hello world!</div>
+export default class Application extends React.Component {
+  render() {
+    return (
+      <div className="application">
+        <Helmet>
+          <meta charSet="utf-8" />
+          <title>Index page title</title>
+        </Helmet>
+        <div>hello world</div>
+        <div>
+          this is a pencent
+        </div>
+      </div>
+    )
+  }
+}
